cc89228520ff0a42d51134cde38a269a0317de8b
CarlosAndresB <carlosandresb@tuum.io>
2018-03-26 21:01:41 +0200

    Update TeleBot.py

--- TeleBot.py
+++ TeleBot.py
@@ -1,20 +1,21 @@
 import time
 import random
 import datetime
+import RPi.GPIO as GPIO
 import telepot
 from telepot.loop import MessageLoop
 
-"""
-After **inserting token** in the source code, run it:
-```
-$ python2.7 diceyclock.py
-```
-[Here is a tutorial](http://www.instructables.com/id/Set-up-Telegram-Bot-on-Raspberry-Pi/)
-teaching you how to setup a bot on Raspberry Pi. This simple bot does nothing
-but accepts two commands:
-- `/roll` - reply with a random integer between 1 and 6, like rolling a dice.
-- `/time` - reply with the current time, like a clock.
-"""
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led = 21
+
+GPIO.setup(led, GPIO.OUT)
+
+def LedOn(val)
+    GPIO.output(led,1)
+    time.sleep(val)
+    GPIO.output(led,0)
 
 def handle(msg):
     chat_id = msg['chat']['id']
@@ -22,8 +23,9 @@ def handle(msg):
 
     print 'Got command: %s' % command
 
-    if command == '/roll':
+    if command == '/On':
         bot.sendMessage(chat_id, random.randint(1,6))
+        LedOn(5)
     elif command == '/time':
         bot.sendMessage(chat_id, str(datetime.datetime.now()))
 
